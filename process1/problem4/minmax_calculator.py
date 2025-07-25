62eb6f1f17ca45d513b2d4d0b44ca64e0801570e
hiho1014 <117014754+hiho1014@users.noreply.github.com>
2025-07-25 20:28:15 +0900

    Add files via upload

--- process1/problem4/minmax_calculator.py
+++ process1/problem4/minmax_calculator.py
@@ -1,10 +1,40 @@
-#인풋 받은 숫자들 공백 기준으로 구분해서 실수로 매핑하고 리스트에 저장 
-num = list(map(float, input("숫자 넣어".split())))
+def main():
+    try:
+        user_input = input("임의의 숫자를 공백으로 구분하여 입력하세요: ")
 
+        if ' ' not in user_input:
+            print("공백을 넣어 숫자를 구분해주세요.")
+            return
 
+        list_n = list(map(float, user_input.split()))
 
+        if len(list_n) < 2:
+            print("최소 두 개 이상의 숫자를 입력해주세요.")
+            return
+        
+        # 입력을 받아 공백 기준으로 나누고 float으로 변환
+        #아래의 코드는 공백 없이 쳤을 때 처리 못함
+        # list_n = list(map(float, input("임의의 숫자를 공백으로 구분하여 입력하세요: ").split()))
 
-# 그 다음 솔트를 쓰자 
+        # if ' ' not in list_n:
+        #     print("공백을 넣어 숫자를 구분해주세요.")
+        #     return
 
+        # 아래의 코드 문법 틀림
+        # if list_n for IndexError : 
+        # print("공백을 입력해주세요")
+        
+        # 리스트를 오름차순 정렬 (min이 맨 앞, max가 맨 뒤로 감)
+        list_n.sort()
 
-# 5번은 랭크나 max과의 절댓값 빼기를 구하자. 
+        # pop으로 최소값(맨 앞), 최대값(맨 뒤) 꺼냄
+        min_val = list_n.pop(0)
+        max_val = list_n.pop(-1)
+
+        print(f"Min: {min_val}, Max: {max_val}")
+
+    except ValueError:
+        print("Invalid input.")
+
+if __name__ == "__main__":
+    main()
